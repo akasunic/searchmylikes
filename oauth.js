3c8352d1fded829f8dfd5c107aaad0dea65541a1
Anna Kasunic <amkasunic@gmail.com>
2020-03-28 19:52:52 -0400

    dummy code (exceeded requests) to collect all videos from pages and add to array

--- oauth.js
+++ oauth.js
@@ -6,13 +6,17 @@
 var pageToken = '';
 var test = 0;
 
+// Probably not with a button. Probably it just happens when you click the extension icon. And then a search bar appears. Which would mean you'd want this as a pop up.
+//maybe brwoser_action is already considered a pop-up, I dunno
 window.onload = function() {
   document.querySelector('button').addEventListener('click', fetchResults(pageToken));
 
 }
 
+var videoArray = [];
+
+
 var fetchResults = function(pageToken){
-  console.log(pageToken);
 
   chrome.identity.getAuthToken({interactive: true}, function(token) {
       var init = {
@@ -23,29 +27,24 @@ var fetchResults = function(pageToken){
           'Content-Type': 'application/json'
         },
         'contentType': 'json'
-      };
-      
+      };  
       test += 1;
 
 
-
-      fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=50&pageToken=' + pageToken, init)
+      fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=50' + pageToken, init)
         .then((response) => response.json())
           .then(function(data){
-            pageToken = data.nextPageToken;
-            
+            console.log(data)
+            pageToken = '&pageToken=' + data.nextPageToken;
+            //Then for each item listed for the page, will add the title results to an array
             console.log(test);
             console.log(pageToken);
-
-
-            // console.log(data);
+            videoArray.push(test);
             
-
-
-            
-            console.log(data.nextPageToken);
-      if(test > 10){
-        console.log('all done');
+      if(test > 22){ //referring to my pages, in particular
+      // if(pageToken == '&pageToken=undefined' || test > 22){ //referring to my pages, in particular
+        console.log('outOfPages');
+        console.log(videoArray);
       }
       else{
         fetchResults(pageToken);
@@ -56,9 +55,7 @@ var fetchResults = function(pageToken){
       
       // console.log(pageToken);
       // fetchResults(pageToken);
-    
-  console.log('running fetchResults');
-  console.log('line10:', pageToken);
+  
       //keep looging next page token until there are no more pages
         //TRYING: https://stackoverflow.com/questions/45008330/how-can-i-use-fetch-in-while-loop
       
@@ -69,9 +66,6 @@ var fetchResults = function(pageToken){
 
  }//ends fetchReults definition
 
-
-
-// fetchResults(pageToken);
 
 
 
