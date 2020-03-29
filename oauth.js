d37dcd2edeecaa814a67762f89f68b7e9514e108
Anna Kasunic <amkasunic@gmail.com>
2020-03-28 20:45:11 -0400

    Have dummy code set up for search functionality to get started. But cant keep doing because I did too many queries cuz Im a dummy

--- oauth.js
+++ oauth.js
@@ -2,21 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+//*************LOOK INTO turning all this into background.js-- like, not the search bar part, but loading the videos. oh or like, not background.js actually-- need to load once a day and store. easier to just keep as is. background.js would still need to be loaded multiple times a day
 'use strict';
 var pageToken = '';
 var test = 0;
 
+
 // Probably not with a button. Probably it just happens when you click the extension icon. And then a search bar appears. Which would mean you'd want this as a pop up.
-//maybe brwoser_action is already considered a pop-up, I dunno
+// maybe brwoser_action is already considered a pop-up, I dunno
 window.onload = function() {
-  document.querySelector('button').addEventListener('click', fetchResults(pageToken));
+//   document.querySelector('button').addEventListener('click', fetchResults(pageToken));
+
+  fetchResults(pageToken);
 
 }
+
 
 var videoArray = [];
 
 
+
+
 var fetchResults = function(pageToken){
+
 
   chrome.identity.getAuthToken({interactive: true}, function(token) {
       var init = {
@@ -40,11 +49,21 @@ var fetchResults = function(pageToken){
             console.log(test);
             console.log(pageToken);
             videoArray.push(test);
+            //WILL WANT TO SEARCH TITLES< BUT WILL LIKELY WANT TO STORE MORE-- at very least, will need video links!
             
-      if(test > 22){ //referring to my pages, in particular
+      if(test > 2){ //referring to my pages, in particular
       // if(pageToken == '&pageToken=undefined' || test > 22){ //referring to my pages, in particular
         console.log('outOfPages');
         console.log(videoArray);
+        //DON'T give button functionality yet? Or don't even load search bar yet, perhaps
+
+        document.querySelector('#myLoadMessage').innerHTML = "Videos loaded. Search away!";
+        // ADD FUNCTIONALITY TO BUTTON HERE (COULD ALSO CHANGE COLOR OF BUTTON OR SOMETHING)
+        //WILL ALSO WANT A MESSAGE FOR ERROR IF VIDEOS NOT PROPERLY LOADING
+
+        //LOOP through all array items (video titles + links) and find out if text matches that of title
+        //possibly look into: fuzzymatching algorithms, javascript
+
       }
       else{
         fetchResults(pageToken);
@@ -65,6 +84,7 @@ var fetchResults = function(pageToken){
 
 
  }//ends fetchReults definition
+
 
 
 
