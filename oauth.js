2723bbdb3af69fe04e18720e5deca8f7094b9abb
Anna Kasunic <amkasunic@gmail.com>
2020-03-29 20:47:06 -0400

    basic search functioning

--- oauth.js
+++ oauth.js
@@ -41,7 +41,7 @@ var fetchResults = function(pageToken){
       fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=50' + pageToken, init)
         .then((response) => response.json())
           .then(function(data){
-            console.log(data);
+            // console.log(data);
             //next page token, for running fetch next time
             pageToken = '&pageToken=' + data.nextPageToken;
             //add each item to the video array for searching
@@ -56,13 +56,52 @@ var fetchResults = function(pageToken){
               var item = {'link': link, 'title': title, 'channel':channel, 'thumbnail': thumbnail};
               videoArray.push(item);
             }
-            
+            //WORKING PRETTY WELL BUT NEXTPAGETOKEN STOPS APPEARING AT VIDEO 989. MAY NEED A WORKAROUND!!
       // if(test > 2){ //to keep testing short
-      if(pageToken == '&pageToken=undefined' || test > 22){ //referring to my pages, in particular
+      if(pageToken == '&pageToken=undefined'){ //referring to my pages, in particular
         console.log(pageToken);
         console.log('outOfPages');
         console.log(videoArray.length);
         document.querySelector('#myLoadMessage').innerHTML = "Videos loaded. Search away!";
+        //add Search functionality to search button now
+        //searching for each word, by channel or title only
+        document.querySelector('#mySearchButton').addEventListener('click', function(){
+          var searchCriteria = document.querySelector('#mySearchBar').value.toUpperCase().split(" ");
+          console.log("searchCriteria length: ", searchCriteria.length);
+
+          for (i=0; i<videoArray.length; i++){
+            var wordCount = 0;
+            var currentLink = videoArray[i];
+            console.log(currentLink);
+            var x; var word;
+            for (x = 0; x<searchCriteria.length; x++){
+              word = searchCriteria[x];
+              console.log(currentLink);
+              if (currentLink.title.toUpperCase().indexOf(word)>-1 || currentLink.channel.toUpperCase().indexOf(word)>-1){
+                wordCount += 1;
+                console.log("word: ", word);
+                console.log(currentLink.title);
+              }
+
+            }
+            console.log('exited word for loop');
+            if (wordCount === searchCriteria.length){
+              //display the link if all search criteria met
+              //could change options for search if desired, but let's start with this
+              //first let's just make page of links
+              //then maybe add thumbnails and channel
+              var div = document.createElement('div');
+              var match = document.createElement('a');
+              match.href = currentLink.link;
+              match.innerHTML = currentLink.title;
+              div.appendChild(match);
+              document.querySelector('#mySearchResults').appendChild(div);
+            }
+          }//ends outer for loop (videoArray loop)
+          
+
+
+        });//ends search button functionality code
         // ADD FUNCTIONALITY TO BUTTON HERE (COULD ALSO CHANGE COLOR OF BUTTON OR SOMETHING)
         //WILL ALSO WANT A MESSAGE FOR ERROR IF VIDEOS NOT PROPERLY LOADING
 
